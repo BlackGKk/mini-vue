52ae363a1fbc1c1fc547d600e4a6dd2ef306eec0
曾小钧 <xjzengt@isoftstone.com>
2023-12-29 15:18:47 +0800

    effect依赖收集\触发,runner,scheduler

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -14,7 +14,46 @@ describe('effect', () => {
         expect(nextAge).toBe(11);
 
         //update
-        // user.age++;
-        // expect(nextAge).toBe(12);
+        user.age++;
+        expect(nextAge).toBe(12);
+    });
+
+    it('runner', () => {
+        //effect(fn) -> function (runner) -> fn -> return
+        let foo = 10;
+        const runner = effect(() => {
+            foo++;
+            return "foo";
+        })
+        expect(foo).toBe(11);
+        const r = runner();
+        expect(foo).toBe(12);
+        expect(r).toBe("foo");
+    });
+
+    it('scheduler', () => {
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({ foo: 1 });
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            { scheduler }
+        );
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        // 在第一次trigger的时候被触发
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        // 未执行run前
+        expect(dummy).toBe(1);
+        // 执行run后
+        run();
+        // 更新
+        expect(dummy).toBe(2);
     });
 });
