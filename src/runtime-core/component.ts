c3a418668e948f4046b810d1b8245acdbc64028f
曾小钧 <xjzengt@isoftstone.com>
2024-04-03 17:53:59 +0800

    实现初始化component主流程

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,6 +1,7 @@
 export function createComponentInstance (vnode) {
     const component = {
-        vnode
+        vnode,
+        type: vnode.type
     }
     return component
 }
@@ -12,9 +13,25 @@ export function setupComponent (instance) {
 }
 
 function setupStatefulComponent (instance) {
-    const Component = instance.vnode.type
+    const Component = instance.type
     const { setup } = Component
     if(setup) {
         const setupResult = setup()
+
+        handleSetupResult(instance,setupResult)
+    }
+}
+
+function handleSetupResult (instance,setupResult) {
+    if(typeof setupResult === 'object') {
+        instance.setupState = setupResult;
+    }
+    finishComponentSetup(instance)
+}
+
+function finishComponentSetup (instance) {
+    const Component = instance.type
+    if(Component.render) {
+        instance.render = Component.render
     }
 }
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -8,16 +8,27 @@ export function render (vnode, container) {
 
 function patch (vnode, container) {
     // 去处理组件
+
+    // 判断是不是element
     processComponent(vnode, container)
 }
 
 function processComponent (vnode, container) {
-    mountComponent(vnode)
+    mountComponent(vnode,container)
 }
 
-function mountComponent (vnode) {
+function mountComponent (vnode, container) {
     const instance = createComponentInstance(vnode)
-    setupComponent(instance);
+    setupComponent(instance)
+    setupRenderEffect(instance, container)
+}
+
+function setupRenderEffect (instance, container) {
+    // 虚拟节点树
+    const subTree = instance.render()
+     // vnode -> patch
+     // vnode -> element -> mountElement
+     patch(subTree, container)
 }
 
 
