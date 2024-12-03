47cc976191d0dceceb9bd00f3299fa1243abdda9
nihaozeng <1934486445@qq.com>
2024-12-03 19:20:54 +0800

    实现transform功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,39 @@
+export function transform (root, options) {
+  const context = createTransformContext(root, options)
+  // 1. 遍历 - 深度优先搜索
+  traverseNode(root, context)
+  // 2. 修改 text content
+}
+
+function createTransformContext(root: any, options: any):any {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+  return context
+}
+
+function traverseNode(node: any, context) {
+  console.log(node);
+  // 遍历调用所有的 nodeTransforms
+  // 把 node 给到 transform
+  // 用户可以对 node 做处理
+  const nodeTransforms = context.nodeTransforms
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
+  }
+
+  traverseChildren(node, context);
+  
+}
+function traverseChildren(node: any, context: any) {
+  const children = node.children;
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i];
+      traverseNode(node, context);
+    }
+  }
+}
+
